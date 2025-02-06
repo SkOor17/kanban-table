3ef92bd9b0967944f7eb307045ce1a82c3dc2c58
thomas CARFANTAN <tcarfantan11@gmail.com>
2025-02-06 15:16:50 +0100

    ediCard + moveCard + editCol + removeCol

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,10 +12,10 @@ export default function Card({
   setDatas: Dispatch<SetStateAction<ColInt[]>>;
 }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(card.title);
 
   const handleDelete = () => {
-    console.log("delete " + card.id);
-
     setDatas((prevDatas) =>
       prevDatas.map((col) => {
         if (col.id === card.nbCol) {
@@ -24,13 +24,30 @@ export default function Card({
             cards: col.cards.filter((c) => c.id !== card.id), // Supprime la carte avec l'ID correspondant
           };
         }
-        return col; // Laisser les autres colonnes inchangées
+        return col;
       })
     );
   };
 
   const handleEdit = () => {
-    console.log("edit" + card.id);
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setDatas((prevDatas) =>
+      prevDatas.map((col) => {
+        if (col.id === card.nbCol) {
+          return {
+            ...col,
+            cards: col.cards.map((c) =>
+              c.id === card.id ? { ...c, title: newTitle } : c
+            ),
+          };
+        }
+        return col;
+      })
+    );
+    setIsEditing(false);
   };
 
   const handleDragStart = (
@@ -38,21 +55,20 @@ export default function Card({
     card: CardInt
   ) => {
     e.dataTransfer.setData("cardId", card.id.toString());
-    console.log("start");
   };
 
   return (
     <div
       className='flex items-center w-full min-h-[50px] h-fit rounded border py-2 px-3 text-wrap cursor-pointer relative bg-white drop-shadow hover:bg-gray-100'
-      onMouseEnter={() => setIsHovered(!isHovered)}
-      onMouseLeave={() => setIsHovered(!isHovered)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       onDragStart={(e) => handleDragStart(e, card)}
       draggable
     >
       <div
         className={`absolute right-3 top-2 flex rounded-md transition-opacity duration-200 ease-in-out ${
           isHovered ? "opacity-100" : "opacity-0"
-        } `}
+        }`}
       >
         <div
           className='w-8 h-8 p-1 rounded-md bg-white flex justify-center items-center border hover:bg-gray-100'
@@ -67,7 +83,20 @@ export default function Card({
           <TrashIcon />
         </div>
       </div>
-      <p>{card.title}</p>
+
+      {isEditing ? (
+        <input
+          type='text'
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          onBlur={handleSave} // Sauvegarde lorsqu'on sort du champ
+          onKeyDown={(e) => e.key === "Enter" && handleSave()} // Sauvegarde avec Entrée
+          autoFocus
+          className='appearance-none border-none outline-none bg-transparent p-0 m-0 w-full'
+        />
+      ) : (
+        <p>{card.title}</p>
+      )}
     </div>
   );
 }
--- app/components/ColumnKanban.tsx
+++ app/components/ColumnKanban.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { CardInt, ColInt } from "@/lib/dico";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import Card from "./Card";
 import AddCard from "./AddCard";
+import EditIcon from "./icons/EditIcon";
+import TrashIcon from "./icons/TrashIcon";
+import clsx from "clsx";
 
 export default function ColumnKanban({
   cards,
@@ -18,26 +21,9 @@ export default function ColumnKanban({
   id: number;
   setDatas: Dispatch<SetStateAction<ColInt[]>>;
 }) {
-  // Set color
-  let mainColor = "";
-  let secondaryColor = "";
-  switch (color) {
-    case "blue":
-      mainColor = `bg-blue-200/70`;
-      secondaryColor = "bg-blue-50";
-      break;
-    case "red":
-      mainColor = "bg-red-200/70";
-      secondaryColor = "bg-red-50";
-      break;
-    case "green":
-      mainColor = "bg-green-200/70";
-      secondaryColor = "bg-green-50";
-      break;
-
-    default:
-      break;
-  }
+  const [isHovered, setIsHovered] = useState(false);
+  const [isEditPopupVisible, setIsEditPopupVisible] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(color);
 
   const getIndicators = () => {
     return Array.from(
@@ -113,21 +99,139 @@ export default function ColumnKanban({
       "avec indicateur",
       idIndicator
     );
+
+    setDatas((prev) => {
+      let isGoingUp = false;
+      let currentCard: CardInt|null = null;
+      let currentColIndex: number;
+      let currentIndex: number
+
+      const res = prev.map((col) => {
+        // On cherche la card pour stocker son ancienne colonne et son ancienne place 
+        const card = col.cards.find((c) => c.id === +cardId);
+        if (card) {
+          currentCard = card;
+          currentColIndex = col.id;
+          currentIndex = col.cards.findIndex((c) => c.id === card.id);
+        }
+
+        // Supprimer la carte de sa colonne d'origine
+        return {
+          ...col,
+          cards: col.cards.filter((c) => c.id !== +cardId),
+        };
+      });
+    
+      if (!currentCard || !idIndicator) return prev;
+    
+      // On insert la card à sa nouvelle place
+      return res.map((col) => {
+        if (col.id === +newColNb && currentCard) {
+          // On -1 dans le seul cas où on monte dans la même colonne
+          // On doit donc vérifier si il monte et si il change de colonne
+          isGoingUp = currentColIndex === col.id && currentIndex < +idIndicator;
+          const index = isGoingUp ? +idIndicator - 1 : +idIndicator;
+          
+          // On insert la card à index
+          const updatedCards = [...col.cards];
+          updatedCards.splice(index, 0, currentCard);
+          return {
+            ...col,
+            cards: updatedCards,
+          };
+        }
+        return col;
+      });
+    });
   };
 
   const handleDragEnd = () => {
     removeOpacityIndicators();
   };
 
+  function handleDelete(): void {
+    setDatas((prevDatas) => {
+      return prevDatas.filter((col) => col.id !== id);
+    });
+  }
+
+  function handleEdit(): void {
+    setIsEditPopupVisible(true);
+  }
+
+  const handleColorChange = (color: "green" | "red" | "blue") => {
+    setSelectedColor(color);
+    setIsEditPopupVisible(false);
+  };
+
   return (
     <div
-      className={`min-w-[300px] min-h-fit h-full flex flex-col py-2 px-3 gap-2 ${secondaryColor} rounded-md`}
+      className={clsx(
+        "min-w-[300px] min-h-fit h-full flex flex-col py-2 px-3 gap-2 rounded-md relative",
+        {
+          "bg-red-50": selectedColor === "red",
+          "bg-blue-50": selectedColor === "blue",
+          "bg-green-50": selectedColor === "green",
+        }
+      )}
       onDragEnter={(e) => handleEnter(e)}
       onDragOver={(e) => handleDragOver(e)}
       onDragEnd={handleDragEnd}
       onDrop={(e) => handleDrop(e, id.toString())}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <p className={`rounded-md ${mainColor} px-3 py-1 w-fit`}>{title}</p>
+      {/* Actions icons */}
+      <div
+        className={`absolute right-3 top-2 flex rounded-md transition-opacity duration-200 ease-in-out ${
+          isHovered ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div
+          className='w-8 h-8 p-1 rounded-md bg-white flex justify-center items-center border hover:bg-gray-100 cursor-pointer'
+          onClick={handleEdit}
+        >
+          <EditIcon />
+        </div>
+        <div
+          className='w-8 h-8 p-1 rounded-md bg-white flex justify-center items-center border hover:bg-gray-100 cursor-pointer'
+          onClick={handleDelete}
+        >
+          <TrashIcon />
+        </div>
+      </div>
+
+      {isEditPopupVisible && (
+        <div className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='bg-white p-4 rounded-md'>
+            <h2>Select a color</h2>
+            <div className='flex gap-2 mt-2'>
+              <button
+                className='bg-red-200 w-8 h-8'
+                onClick={() => handleColorChange("red")}
+              ></button>
+              <button
+                className='bg-blue-200 w-8 h-8'
+                onClick={() => handleColorChange("blue")}
+              ></button>
+              <button
+                className='bg-green-200 w-8 h-8'
+                onClick={() => handleColorChange("green")}
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <p
+        className={clsx("rounded-md px-3 py-1 w-fit", {
+          "bg-red-200": selectedColor === "red",
+          "bg-blue-200": selectedColor === "blue",
+          "bg-green-200": selectedColor === "green",
+        })}
+      >
+        {title}
+      </p>
       <div>
         {cards.map((c, index) => {
           return (
